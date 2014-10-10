2d4e93f0978c8badf4ed6918010cbe1da90664d5
Sean Cross <xobs@kosagi.com>
2014-10-10 16:40:30 +0800

    examples: Update package versions, package lists
    As the list of packages we're installing evolves, update the packages
    that get installed as part of the example list.
    
    Also, require the user specify which device to blow away.  That's important.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- mmc-install.sh
+++ mmc-install.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
-echo "Constructing a disk image on /dev/mmcblk1"
+if [ -z $1 ]
+then
+	echo "Usage: $0 [device]"
+	echo "E.g. $0 /dev/mmcblk1"
+	exit 1
+fi
+
+echo "Constructing a disk image on $1"
 exec sudo ./novena-image.sh \
-	-d /dev/mmcblk1 \
+	-d $1 \
 	-t mmc \
 	-s jessie \
 	-a pulseaudio-novena_1.0-1_all.deb \
 	-a irqbalance_0.56-1ubuntu4-rmk1_armhf.deb \
-	-a novena-disable-ssp_1.0-1_armhf.deb \
-	-a u-boot-novena_2014.10-novena-rc1_armhf.deb \
+	-a novena-disable-ssp_1.1-1_armhf.deb \
+	-a u-boot-novena_2014.10-novena-rc2_armhf.deb \
 	-l "sudo openssh-server ntp ntpdate dosfstools novena-eeprom \
-            xserver-xorg-video-modesetting task-xfce-desktop \
-	    hicolor-icon-theme gnome-icon-theme tango-icon-theme keychain \
-	    avahi-daemon avahi-dnsconfd libnss-mdns dosfstools debootstrap \
-	    python build-essential xscreensaver vlc vim emacs \
+            xserver-xorg-video-modesetting arandr \
+	    hicolor-icon-theme gnome-icon-theme keychain \
+	    avahi-daemon avahi-dnsconfd libnss-mdns debootstrap \
+	    python build-essential xscreensaver vim emacs \
 	    x11-xserver-utils usbutils unzip xz-utils subversion git make \
 	    screen tmux read-edid powertop powermgmt-base pavucontrol \
 	    p7zip-full paprefs pciutils nmap ntfs-3g network-manager-vpnc \
-	    network-manager-pptp network-manager-openvpn \
-	    network-manager-iodine libreoffice icedove iceweasel gnupg2 git \
+	    network-manager-pptp network-manager-openvpn bash-completion \
+	    network-manager-iodine xchat icedove iceweasel gnupg2 \
 	    git-email git-man fuse enigmail dc curl clang bridge-utils \
 	    bluez bluez-tools bluez-hcidump bison bc automake autoconf \
 	    pidgin alsa-utils i2c-tools hwinfo android-tools-adb \
-	    android-tools-fastboot android-tools-fsutils"
+	    android-tools-fastboot android-tools-fsutils \
+	    xfce4-goodies xfce4-power-manager xfce4-mixer xfce4-terminal \
+	    mousepad orage libreoffice-gtk dbus-x11 quodlibet evince-gtk \
+	    tango-icon-theme network-manager-gnome synaptic \
+	    gnome-orca ncurses-dev gdb lzop lzop gawk bison g++ gcc"
--- sata-install.sh
+++ sata-install.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-echo "Constructing a disk image on /dev/sdb"
+if [ -z $1 ]
+then
+	echo "Usage: $0 [device]"
+	echo "E.g. $0 /dev/mmcblk1"
+	exit 1
+fi
+
+echo "Constructing a disk image on $1"
 exec sudo ./novena-image.sh \
-	-d /dev/sdb \
+	-d $1 \
 	-t sata \
 	-s jessie \
 	-a pulseaudio-novena_1.0-1_all.deb \
 	-a irqbalance_0.56-1ubuntu4-rmk1_armhf.deb \
-	-a novena-disable-ssp_1.0-1_armhf.deb \
-	-a u-boot-novena_2014.10-novena-rc1_armhf.deb \
+	-a novena-disable-ssp_1.1-1_armhf.deb \
+	-a u-boot-novena_2014.10-novena-rc2_armhf.deb \
 	-l "sudo openssh-server ntp ntpdate dosfstools btrfs-tools \
 	    novena-eeprom xserver-xorg-video-modesetting task-xfce-desktop \
 	    hicolor-icon-theme gnome-icon-theme tango-icon-theme keychain \
-	    avahi-daemon avahi-dnsconfd libnss-mdns btrfs-tools dosfstools \
+	    avahi-daemon avahi-dnsconfd libnss-mdns btrfs-tools \
 	    parted debootstrap python build-essential xscreensaver vlc vim \
 	    emacs x11-xserver-utils usbutils unzip apt-file xz-utils \
-	    subversion git make screen tmux read-edid powertop powermgmt-base \
+	    subversion make screen tmux read-edid powertop powermgmt-base \
 	    pavucontrol p7zip-full paprefs pciutils nmap ntfs-3g \
 	    network-manager-vpnc network-manager-pptp network-manager-openvpn \
 	    network-manager-iodine mplayer2 libreoffice imagemagick icedove \
@@ -22,4 +29,5 @@ exec sudo ./novena-image.sh \
 	    enigmail dc curl clang bridge-utils bluez bluez-tools \
 	    bluez-hcidump bison bc automake autoconf pidgin alsa-utils verilog \
 	    i2c-tools hwinfo android-tools-adb android-tools-fastboot \
-	    android-tools-fsutils libcap-ng0 libglib2.0-0"
+	    android-tools-fsutils bash-completion kicad ncurses-dev gdb lzop \
+	    gawk bison g++ gcc"
