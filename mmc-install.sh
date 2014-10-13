e9ff423202d9826aa9e6b5347ebafa83a089f00e
Sean Cross <xobs@kosagi.com>
2014-10-13 19:09:12 +0800

    mmc-install: Add more useful packages
    This is from a diff with a working system.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- mmc-install.sh
+++ mmc-install.sh
@@ -20,18 +20,21 @@ time sudo ./novena-image.sh \
 	-l "sudo openssh-server ntp ntpdate dosfstools novena-eeprom \
             xserver-xorg-video-modesetting arandr user-setup vim emacs \
 	    hicolor-icon-theme gnome-icon-theme keychain locales \
-	    avahi-daemon avahi-dnsconfd libnss-mdns debootstrap \
-	    python build-essential xscreensaver console-data \
+	    avahi-daemon avahi-dnsconfd libnss-mdns debootstrap psutils \
+	    python build-essential xscreensaver console-data zip tcpdump \
 	    x11-xserver-utils usbutils unzip xz-utils subversion git make \
 	    screen tmux read-edid powertop powermgmt-base pavucontrol \
 	    p7zip-full paprefs pciutils nmap ntfs-3g network-manager-vpnc \
 	    network-manager-pptp network-manager-openvpn bash-completion \
-	    network-manager-iodine hexchat icedove iceweasel gnupg2 \
+	    network-manager-iodine hexchat icedove iceweasel gnupg2 unp \
 	    git-email git-man fuse enigmail dc curl clang bridge-utils \
 	    bluez bluez-tools bluez-hcidump bison bc automake autoconf \
-	    pidgin alsa-utils i2c-tools hwinfo android-tools-adb \
+	    pidgin alsa-utils i2c-tools hwinfo android-tools-adb unrar \
 	    android-tools-fastboot android-tools-fsutils smartmontools \
 	    xfce4-goodies xfce4-power-manager xfce4-mixer xfce4-terminal \
 	    mousepad orage dbus-x11 quodlibet evince-gtk irssi strace \
 	    tango-icon-theme network-manager-gnome synaptic pkg-config \
-	    gnome-orca ncurses-dev gdb lzop lzop gawk bison g++ gcc flex"
+	    gnome-orca ncurses-dev gdb lzop lzop gawk bison g++ gcc flex \
+	    wireshark pm-utils qalc qalculatei memtester locate mousetweaks \
+	    iptraf iperf iotop initramfs-tools gnupg-agent extfat-fuse \
+	    extfat-utils dict ark aptitude"
