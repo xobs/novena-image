bbb01cf95134bc3628371c19985e7e10abbbbb48
jogi <jweinbuch@gmx.ch>
2015-04-10 23:55:20 +0200

    Fixed uEnv.txt generation and thinned the encrypted install package list

--- encrypted-sata-install.sh
+++ encrypted-sata-install.sh
@@ -15,24 +15,9 @@ time sudo /bin/bash -x ./novena-image.sh \
 	-k kosagi.key \
 	-a u-boot-novena_2014.10-novena-rc12_armhf.deb \
 	-a irqbalance-imx_0.56-1ubuntu4-rmk1_armhf.deb \
-	-a libdrm-armada2_2.0.2-1_armhf.deb \
-	-a libdrm-armada2-dbg_2.0.2-1_armhf.deb \
-	-a libdrm-armada-dev_2.0.2-1_armhf.deb \
-	-a libetnaviv_0.0.0-r11_armhf.deb \
-	-a novena-usb-hub_1.0-1_armhf.deb \
-	-a libetnaviv-dbg_0.0.0-r11_armhf.deb \
-	-a libetnaviv-dev_0.0.0-r11_armhf.deb \
-	-a linux-headers-novena_1.14-r1_armhf.deb \
-	-a linux-image-novena_1.14-r1_armhf.deb \
-	-a novena-disable-ssp_1.1-1_armhf.deb \
 	-a novena-eeprom_2.1-1_armhf.deb \
-	-a novena-eeprom-gui_1.2-r1_armhf.deb \
 	-a kosagi-repo_1.0-r1_all.deb \
 	-a novena-firstrun_1.4-r1_all.deb \
-	-a xorg-novena_1.2-r1_all.deb \
-	-a xserver-xorg-video-armada_0.0.1-r1_armhf.deb \
-	-a xserver-xorg-video-armada-dbg_0.0.1-r1_armhf.deb \
-	-a xserver-xorg-video-armada-etnaviv_0.0.1-r1_armhf.deb \
 	-l "sudo openssh-server ntp ntpdate dosfstools novena-eeprom \
             xserver-xorg-video-modesetting arandr user-setup vim emacs \
 	    keychain locales evtest libbluetooth3 \
